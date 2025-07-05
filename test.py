fa493f50ab27709e00575deb31141c0712753ff6
ispranaypink <117278249+ispranaypink@users.noreply.github.com>
2025-07-05 13:53:32 +0530

    Removing test files

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-API_KEY = "AIzaSyAc89Dp1Njs1ROy05tfConahro6IAUFRv4"
-address = "New York, NY"
-url = "https://maps.googleapis.com/maps/api/geocode/json"
-params = {"address": address, "key": API_KEY}
-resp = requests.get(url, params=params)
-print(resp.json())
